c892834b3730e11787c5c4c46e9acf477b6c06fb
jh <jwhooper@gmail.com>
2018-12-18 16:24:59 -0500

    up

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(require('./routes/index.routes'))
 // First route
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello world' })
-})
-// Starting server
-app.listen('1337')
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
+});
